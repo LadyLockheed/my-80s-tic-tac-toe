166aef9d9f94df720fcd0872cbed5f5a2cb09687
Karin Karlsson <karin.drake@gmail.com>
2024-10-24 16:07:29 +0200

    Import Board component to app, add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { Board } from "./Components"
+import './App.css'
 
 function App() {
 
 
   return (
-    <>
-    <Board/>
-    </>
+
+    <div className='centered-container'>
+      <h1>Tic-Tac-Toe</h1>
+  
+      <Board/>
+  
+
+      <h1>Winner is: some winner</h1>
+  
+
+    </div>
+
   )
 }
 
