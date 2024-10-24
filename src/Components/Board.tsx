1f1df885d2fd3d445558bbd11490544695d3cd1d
Karin Karlsson <karin.drake@gmail.com>
2024-10-24 16:07:07 +0200

    squares in board

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,6 +1,28 @@
+import './board.css'
+
 const Board = () => {
   return (
-    <div>Board</div>
+    <div className='board'>
+
+      <div className="squares-row">
+        <div className='square'></div>
+        <div className='square'></div>
+        <div className='square'></div>
+      </div>
+
+      <div className="squares-row">
+        <div className='square'>4</div>
+        <div className='square'>5</div>
+        <div className='square'>6</div>
+      </div>
+
+      <div className="squares-row">
+        <div className='square'>7</div>
+        <div className='square'>8</div>
+        <div className='square'>9</div>
+      </div>
+
+    </div>
   )
 }
 export default Board
